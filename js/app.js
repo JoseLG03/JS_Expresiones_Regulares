cecd9c9b4988b248c19c2a1ccddd01a562f4c4c4
Jose <antares069@gmail.com>
2020-08-09 18:11:59 -0500

    abreviaciones

--- js/app.js
+++ js/app.js
@@ -5,4 +5,18 @@ var expresion2 = /abc/;
 
 var parrafo = /abc \/n dfe/;
 
-console.log(expresion2.test('El lema es Abc'));
+var expresion3 = /[0-9a-zA-Z]/;
+
+console.log(expresion3.test('Sucedio en el verano de 1998.'));
+
+/*Grupo de caracteres especiales
+
+\d Cualquier número
+\w Cualquier número o letra
+\s Cualquier espacio en blanco
+\D Cualquier caracter que no sea un número
+\W Cualquier caracter que no se alfanúmerico
+\S Cualquier caracter que no sea un espacio en blanco
+.  Cualquier caracter excepto nuevas lineas
+
+*/
