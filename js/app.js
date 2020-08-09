506d52efdd3c546c284e9f7a79d00257abc72886
Jose <antares069@gmail.com>
2020-08-09 18:01:04 -0500

    primer caso

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+/*Basicas*/
+var expresion1 = new RegExp('abc');
+
+var expresion2 = /abc/;
+
+var parrafo = /abc \/n dfe/;
+
+console.log(expresion2.test('El lema es Abc'));
